1e58b53a1c832f85154b3dcca028f2ba24ec0eb7
ivar van den berg <squall_leonhart_gh@hotmail.com>
2018-10-12 12:29:13 +0200

    Add project files.

---
+++ DictionairyTest/DictionaryManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTest
+{
+    class DictionaryManager
+    {
+        Dictionary<string, string> testDictionary = new Dictionary<string, string>();
+
+        public void Add(string key, string value/*, Dictionary<string, string> dictionary*/)
+        {
+            if (!testDictionary.ContainsKey(key))
+            {
+                testDictionary.Add(key, value);
+            }
+
+            else { throw new ArgumentException("Dictionary allready contains key"); }
+        }
+
+
+        public IEnumerable<string> Test()
+        {
+            AutoAdder(testDictionary);
+            foreach (var n in testDictionary)
+            {
+                yield return n.Value;
+            }
+        }
+
+        public void AutoAdder(Dictionary<string, string> dictionairy)
+        {
+            for (var i = 0; i < 20; i++)
+            {
+                Add(Convert.ToString(i), Convert.ToString(i));
+            }
+        }
+
+
+    }
+}
---
+++ DictionairyTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionaryManager = new DictionaryManager();
+            var result = dictionaryManager.Test();
+
+            foreach (var n in result)
+            {
+                Console.WriteLine(n);
+            }
+
+          
+
+        }
+    }
+}
